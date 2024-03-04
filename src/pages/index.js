72ca68775a83abba8fa4174fb5a9a4259794c59d
Diogo Magalhães <d.magalhaes@ua.pt>
2024-03-04 10:21:48 +0000

    Removed images from old project, some css improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         {/* Illustration bg */}
         <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1
         " aria-hidden="true">
-          <div className="dark:hidden">
+          <div className="dark:hidden hideOnMobile">
             <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
               <defs>
                 <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
